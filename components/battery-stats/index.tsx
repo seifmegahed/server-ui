c55a0a12e765b0e9590ff8d9a1321a2a9403d1aa
Seif Megahed <seifmegahed@me.com>
2024-09-10 10:16:54 +0000

    updated battery-stats component to use latest battery icon and accept values as props

--- components/battery-stats/index.tsx
+++ components/battery-stats/index.tsx
@@ -1,47 +1,32 @@
-import { getBattery } from "@/server/actions";
-import {
-  BatteryFull,
-  BatteryCharging,
-  Battery,
-  BatteryMedium,
-  BatteryLow,
-} from "lucide-react";
+import BatteryIcon from "@/icons/BatteryIcon";
 
-async function BatteryStats() {
-  const battery = await getBattery();
-  const chargeRatio = Number(battery.charge) / Number(battery.chargeFull);
-  const chargeStep = Math.round(chargeRatio * 3);
-  const charge = Math.max(Math.min(Math.round(chargeRatio * 100), 100), 0);
+const batteryStatus = {
+  Charging: "Charging\n",
+  Full: "Full\n",
+};
+
+export type BatteryDataType = {
+  status: string;
+  charge: number;
+};
+
+function BatteryStats({ battery }: { battery: BatteryDataType }) {
   return (
     <div className="flex gap-2 items-center">
-      <BatteryIcon status={battery.status} chargeStep={chargeStep} />
-      <div className="text-md">{charge}%</div>
+      <div className="text-sm">
+        {battery.status === batteryStatus.Full ? 100 : battery.charge}%
+      </div>
+      <BatteryIcon
+        percentage={
+          battery.status === batteryStatus.Full ? 100 : battery.charge
+        }
+        charging={
+          battery.status === batteryStatus.Charging ||
+          battery.status === batteryStatus.Full
+        }
+      />
     </div>
   );
-}
-
-function BatteryIcon({
-  status,
-  chargeStep,
-}: {
-  status: string;
-  chargeStep: number;
-}) {
-  switch (status) {
-    case "Charging\n":
-      return <BatteryCharging />;
-    default:
-      switch (chargeStep) {
-        case 3:
-          return <BatteryFull />;
-        case 2:
-          return <BatteryMedium />;
-        case 1:
-          return <BatteryLow />;
-        default:
-          return <Battery />;
-      }
-  }
 }
 
 export default BatteryStats;
